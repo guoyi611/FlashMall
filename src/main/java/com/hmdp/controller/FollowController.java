8974d6f0bccbd8154c027e449ad0cb1ce77eac1b
shiguoyu <15813277123@163.com>
2026-08-09 15:39:37 +0800

    2.5 通过关注时插入set集合，查交集来实现共同关注功能

--- src/main/java/com/hmdp/controller/FollowController.java
+++ src/main/java/com/hmdp/controller/FollowController.java
@@ -32,4 +32,10 @@ public class FollowController {
     public Result isFollow(@PathVariable("id") Long followUserId) {
         return followService.isFollow(followUserId);
     }
+
+    //查询共同关注
+    @GetMapping("/common/{id}")
+    public Result followCommons(@PathVariable("id") Long id){
+        return followService.followCommons(id);
+    }
 }
